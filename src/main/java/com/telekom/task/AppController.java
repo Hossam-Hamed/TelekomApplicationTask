07458a4a8d883e06c5fc0004ba6323615cefe304
Hossam Hamed <hamed@fim.uni-passau.de>
2022-12-05 23:28:06 +0100

    edit build.gradle to create JAR + documentation + clean up

--- src/main/java/com/telekom/task/AppController.java
+++ src/main/java/com/telekom/task/AppController.java
@@ -1,14 +1,15 @@
 package com.telekom.task;
 
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
-
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,27 +24,56 @@ public class AppController {
         return "index";
     }
 
+    /***
+     * CSR file uplaod endpoint
+     *
+     * @param model
+     * @param multipartFile uploaded file
+     * @return redirects to csrInfoViewer.html
+     * @throws IOException
+     */
     @PostMapping("/upload")
     public String fileupload(Model model, @RequestParam("file") MultipartFile multipartFile) throws IOException {
         PKCS10CertificationRequest csr = csrParser.parseCSRFromFile(multipartFile);
         return responseHandler(model, csr);
     }
 
+    /***
+     * CSR PEM as text endpoint
+     * @param model
+     * @param csrString CSR PEM format
+     * @return redirects to csrInfoViewer.html
+     * @throws IOException
+     */
+
     @PostMapping("/submitCSR")
     public String formSubmit(Model model, @ModelAttribute("csr") String csrString) throws IOException {
-        //remove blanks
-        csrString = csrString.substring(0,35)+"\n"+csrString.substring(36,csrString.length()-34).replaceAll(" ","")+"\n"+csrString.substring(csrString.length()-33);
+
+        //remove blanks within CSR String which cause parsing errors
+        csrString = csrString.substring(0, 35) + "\n" + csrString.substring(36, csrString.length() - 34).replaceAll(" ", "") + "\n" + csrString.substring(csrString.length() - 33);
         PKCS10CertificationRequest csr = csrParser.parseCSRFromString(csrString);
         return responseHandler(model, csr);
 
     }
 
+    /***
+     * Prepares response containing parsed CSR information
+     * @param model
+     * @param csr Parsed {@link PKCS10CertificationRequest} CSR object
+     * @return
+     */
     String responseHandler(Model model, PKCS10CertificationRequest csr) {
 
+        //Add information map to model
         model.addAttribute("csrInfo", csrInfo(csr));
         return "csrInfoViewer";
     }
 
+    /***
+     * creates parsed CSR information in a map
+     * @param csr
+     * @return information map
+     */
     Map<String, Object> csrInfo(PKCS10CertificationRequest csr) {
 
         Map<String, Object> response = new HashMap<>();
--- src/main/java/com/telekom/task/CSRParser.java
+++ src/main/java/com/telekom/task/CSRParser.java
@@ -1,89 +1,61 @@
 package com.telekom.task;
 
-import org.bouncycastle.asn1.ASN1Set;
-
 import org.bouncycastle.asn1.pkcs.Attribute;
-import org.bouncycastle.asn1.x500.RDN;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
-import org.bouncycastle.pkcs.PKCS10CertificationRequest;
-
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.operator.DefaultAlgorithmNameFinder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
-
-import static java.util.Collections.replaceAll;
 
 public class CSRParser {
 
-    ArrayList<PKCS10CertificationRequest> parsedCSRs;
 
-    public CSRParser() {
-        parsedCSRs = new ArrayList<>();
-    }
-
-    // Parse CSR from string
+    // Parse CSR in PEM format from string
     public PKCS10CertificationRequest parseCSRFromString(String pemCSR) throws IOException {
         ByteArrayInputStream in = new ByteArrayInputStream(pemCSR.getBytes());
         Reader bufferedReader = new BufferedReader(new InputStreamReader(in));
         PEMParser pemReader = new PEMParser(bufferedReader);
         PKCS10CertificationRequest parsedCSR = (PKCS10CertificationRequest) pemReader.readObject();
-        parsedCSRs.add(parsedCSR);
         return parsedCSR;
     }
+
     // Parse CSR from Multipartfile
     public PKCS10CertificationRequest parseCSRFromFile(MultipartFile multipartFile) throws IOException {
         Reader bufferedReader = new InputStreamReader(multipartFile.getInputStream());
         PEMParser pemReader = new PEMParser(bufferedReader);
         PKCS10CertificationRequest parsedCSR = (PKCS10CertificationRequest) pemReader.readObject();
-        parsedCSRs.add(parsedCSR);
         return parsedCSR;
     }
 
+    //gets CSR's Subject X500 name
     public String getSubjectName(PKCS10CertificationRequest csr) {
         return csr.getSubject().toString();
-
-
     }
 
+    //gets CSR's attributes
     public String getAttributes(PKCS10CertificationRequest csr) {
         Attribute[] attributes = csr.getAttributes();
         //TODO find SAN in attributes
         return Arrays.toString(attributes);
     }
 
+    // Gets CSR's public key algorithm
     public String getPKAlg(PKCS10CertificationRequest csr) {
+        // translates oid to algorithm name
         DefaultAlgorithmNameFinder defaultAlgorithmNameFinder = new DefaultAlgorithmNameFinder();
+
+        // finds pk algorithm oid
         AlgorithmIdentifier algorithmIdentifier = csr.getSubjectPublicKeyInfo().getAlgorithm();
         return defaultAlgorithmNameFinder.getAlgorithmName(algorithmIdentifier);
     }
 
     public static void main(String[] args) throws IOException {
         CSRParser csrParser = new CSRParser();
-        String sampleCSR =
-                "-----BEGIN CERTIFICATE REQUEST-----\n" +
-                        "MIICvDCCAaQCAQAwdzELMAkGA1UEBhMCVVMxDTALBgNVBAgMBFV0YWgxDzANBgNV\n" +
-                        "BAcMBkxpbmRvbjEWMBQGA1UECgwNRGlnaUNlcnQgSW5jLjERMA8GA1UECwwIRGln\n" +
-                        "aUNlcnQxHTAbBgNVBAMMFGV4YW1wbGUuZGlnaWNlcnQuY29tMIIBIjANBgkqhkiG\n" +
-                        "9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8+To7d+2kPWeBv/orU3LVbJwDrSQbeKamCmo\n" +
-                        "wp5bqDxIwV20zqRb7APUOKYoVEFFOEQs6T6gImnIolhbiH6m4zgZ/CPvWBOkZc+c\n" +
-                        "1Po2EmvBz+AD5sBdT5kzGQA6NbWyZGldxRthNLOs1efOhdnWFuhI162qmcflgpiI\n" +
-                        "WDuwq4C9f+YkeJhNn9dF5+owm8cOQmDrV8NNdiTqin8q3qYAHHJRW28glJUCZkTZ\n" +
-                        "wIaSR6crBQ8TbYNE0dc+Caa3DOIkz1EOsHWzTx+n0zKfqcbgXi4DJx+C1bjptYPR\n" +
-                        "BPZL8DAeWuA8ebudVT44yEp82G96/Ggcf7F33xMxe0yc+Xa6owIDAQABoAAwDQYJ\n" +
-                        "KoZIhvcNAQEFBQADggEBAB0kcrFccSmFDmxox0Ne01UIqSsDqHgL+XmHTXJwre6D\n" +
-                        "hJSZwbvEtOK0G3+dr4Fs11WuUNt5qcLsx5a8uk4G6AKHMzuhLsJ7XZjgmQXGECpY\n" +
-                        "Q4mC3yT3ZoCGpIXbw+iP3lmEEXgaQL0Tx5LFl/okKbKYwIqNiyKWOMj7ZR/wxWg/\n" +
-                        "ZDGRs55xuoeLDJ/ZRFf9bI+IaCUd1YrfYcHIl3G87Av+r49YVwqRDT0VDV7uLgqn\n" +
-                        "29XI1PpVUNCPQGn9p/eX6Qo7vpDaPybRtA2R7XLKjQaF9oXWeCUqy1hvJac9QFO2\n" +
-                        "97Ob1alpHPoZ7mWiEuJwjBPii6a9M9G30nUo39lBi1w=\n"
-//                ;
-                        +
-                        "-----END CERTIFICATE REQUEST-----";
+        String sampleCSR = "-----BEGIN CERTIFICATE REQUEST-----\n" + "MIICvDCCAaQCAQAwdzELMAkGA1UEBhMCVVMxDTALBgNVBAgMBFV0YWgxDzANBgNV\n" + "BAcMBkxpbmRvbjEWMBQGA1UECgwNRGlnaUNlcnQgSW5jLjERMA8GA1UECwwIRGln\n" + "aUNlcnQxHTAbBgNVBAMMFGV4YW1wbGUuZGlnaWNlcnQuY29tMIIBIjANBgkqhkiG\n" + "9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8+To7d+2kPWeBv/orU3LVbJwDrSQbeKamCmo\n" + "wp5bqDxIwV20zqRb7APUOKYoVEFFOEQs6T6gImnIolhbiH6m4zgZ/CPvWBOkZc+c\n" + "1Po2EmvBz+AD5sBdT5kzGQA6NbWyZGldxRthNLOs1efOhdnWFuhI162qmcflgpiI\n" + "WDuwq4C9f+YkeJhNn9dF5+owm8cOQmDrV8NNdiTqin8q3qYAHHJRW28glJUCZkTZ\n" + "wIaSR6crBQ8TbYNE0dc+Caa3DOIkz1EOsHWzTx+n0zKfqcbgXi4DJx+C1bjptYPR\n" + "BPZL8DAeWuA8ebudVT44yEp82G96/Ggcf7F33xMxe0yc+Xa6owIDAQABoAAwDQYJ\n" + "KoZIhvcNAQEFBQADggEBAB0kcrFccSmFDmxox0Ne01UIqSsDqHgL+XmHTXJwre6D\n" + "hJSZwbvEtOK0G3+dr4Fs11WuUNt5qcLsx5a8uk4G6AKHMzuhLsJ7XZjgmQXGECpY\n" + "Q4mC3yT3ZoCGpIXbw+iP3lmEEXgaQL0Tx5LFl/okKbKYwIqNiyKWOMj7ZR/wxWg/\n" + "ZDGRs55xuoeLDJ/ZRFf9bI+IaCUd1YrfYcHIl3G87Av+r49YVwqRDT0VDV7uLgqn\n" + "29XI1PpVUNCPQGn9p/eX6Qo7vpDaPybRtA2R7XLKjQaF9oXWeCUqy1hvJac9QFO2\n" + "97Ob1alpHPoZ7mWiEuJwjBPii6a9M9G30nUo39lBi1w=\n" + "-----END CERTIFICATE REQUEST-----";
 
         PKCS10CertificationRequest parsedCsr = csrParser.parseCSRFromString(sampleCSR);
         System.out.println(csrParser.getSubjectName(parsedCsr));
