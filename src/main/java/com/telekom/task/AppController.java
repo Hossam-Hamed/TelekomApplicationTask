393cbbfa5a5fdd89fd9f4709ae6262df1f0d6a77
Hossam Hamed <hamed@fim.uni-passau.de>
2022-12-05 17:35:41 +0100

    added form submission

--- src/main/java/com/telekom/task/AppController.java
+++ src/main/java/com/telekom/task/AppController.java
@@ -1,15 +1,12 @@
 package com.telekom.task;
 
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.stereotype.Controller;
+
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -22,28 +19,36 @@ public class AppController {
     CSRParser csrParser = new CSRParser();
 
     @RequestMapping("/")
-    public String landingPage(Model model){
+    public String landingPage(Model model) {
         return "index";
     }
+
     @PostMapping("/upload")
-    public ResponseEntity<Object> fileupload(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+    public String fileupload(Model model, @RequestParam("file") MultipartFile multipartFile) throws IOException {
         PKCS10CertificationRequest csr = csrParser.parseCSRFromFile(multipartFile);
-        return csrInfo(csr);
+        return responseHandler(model, csr);
     }
 
     @PostMapping("/submitCSR")
-    public ResponseEntity<Object> formSubmit(@RequestParam("csr") String csrString) throws IOException {
+    public String formSubmit(Model model, @ModelAttribute("csr") String csrString) throws IOException {
+        //remove blanks
+        csrString = csrString.substring(0,35)+"\n"+csrString.substring(36,csrString.length()-34).replaceAll(" ","")+"\n"+csrString.substring(csrString.length()-33);
         PKCS10CertificationRequest csr = csrParser.parseCSRFromString(csrString);
-        return csrInfo(csr);
+        return responseHandler(model, csr);
 
     }
 
-    ResponseEntity<Object> csrInfo(PKCS10CertificationRequest csr){
+    String responseHandler(Model model, PKCS10CertificationRequest csr) {
+
+        model.addAllAttributes(csrInfo(csr));
+        return "csrInfoViewer";
+    }
+
+    Map<String, Object> csrInfo(PKCS10CertificationRequest csr) {
 
         Map<String, Object> response = new HashMap<>();
         response.put("Subject", csr.getSubject().toString());
         response.put("Attributes", Arrays.toString(csr.getAttributes()));
-        return new ResponseEntity<>(response, HttpStatus.OK);
-
+        return response;
     }
 }
